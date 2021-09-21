27b7fcd104fd5fc02a2d4fd5201946e56f7c69e2
chogyejin <whrpwls96@naver.com>
2021-09-21 15:12:54 +0900

    link test

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function About() {
+  return (
+    <>
+      <p>about</p>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  );
+}
