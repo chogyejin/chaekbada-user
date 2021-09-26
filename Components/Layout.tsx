533011f84ad1cc2820e31dbb64ba4676b7763c9b
chogyejin <whrpwls96@naver.com>
2021-09-26 19:10:59 +0900

    login.tsx, signup.tsx 추가

--- Components/Layout.tsx
+++ Components/Layout.tsx
@@ -1,24 +1,6 @@
 import Header from './Header';
 import Footer from './Footer';
 
-const styles = {
-  layout: {
-    display: 'flex',
-    width: '100%',
-    height: '100%',
-    flexDirection: 'column',
-  },
-  Header: {
-    height: 60,
-  },
-  main: {
-    flex: 1,
-  },
-  Footer: {
-    height: 60,
-  },
-};
-
 export default function Layout(props: any) {
   return (
     <>
