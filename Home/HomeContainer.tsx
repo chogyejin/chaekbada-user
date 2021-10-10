c890c45bc36609b013a82486f397967787b22e4a
chogyejin <whrpwls96@naver.com>
2021-10-11 01:34:33 +0900

    HomeContainer.tsx href url 수정

--- Home/HomeContainer.tsx
+++ Home/HomeContainer.tsx
@@ -1,20 +1,17 @@
 import Link from 'next/link';
 import styles from '/styles/HomeContainer.module.scss';
 const HomeContainer = () => {
-    return (
-        <>
-          
-          <div className={styles.container}>
-          <Link href="/postList">게시판 가기</Link>
-          
-          
-            <Link href="/login">로그인 가기</Link>
-          
-          
-            <Link href="/signup">회원가입 가기</Link>
-          </div>
-        </>
-      );
-  }
-  
-  export default HomeContainer
+  return (
+    <>
+      <div className={styles.container}>
+        <Link href="/PostList">게시판 가기</Link>
+
+        <Link href="/Login">로그인 가기</Link>
+
+        <Link href="/SignUp">회원가입 가기</Link>
+      </div>
+    </>
+  );
+};
+
+export default HomeContainer;
