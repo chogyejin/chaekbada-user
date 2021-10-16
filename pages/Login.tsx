2983a8d09545db12f0f8296420a8de9f5d73c210
kyunghoon1 <32170331@dankook.ac.kr>
2021-10-16 14:47:40 +0900

    no message

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,7 +1,86 @@
-export default function Login() {
+import React, { useRef, useState } from 'react';
+import Link from 'next/link';
+
+export default function SignUp() {
+  const [isLoading, setIsLoading] = useState(false);
+  // const emailRef = useRef<HTMLInputElement>(null);
+  // const pwdRef = useRef<HTMLInputElement>(null);
+  //const nameRef= useRef<HTMLInputElement>(null);
+  // const pwdCheckRef = useRef<HTMLInputElement>(null);
+  //const school_nameRef = useRef<HTMLInputElement>(null);
+  //const loginemailRef=useRef<HTMLInputElement>(null);
+  //const loginpasswordRef=useRef<HTMLInputElement>(null);
+
+ 
+  const[loginemail,loginEmail] = useState<string>('');
+  const[loginpassword,loginPassword]=useState<string>('');
+
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    //로딩중이 아니고 입력 다 받으면 보내기
+    if (!isLoading && loginemail && loginpassword) {
+      setIsLoading(true);
+
+      
+      console.log(loginemail);
+      console.log(loginpassword);
+      
+    }
+    setIsLoading(false);
+  }
+
   return (
-    <div>
-      <div>로그인 페이지입니다</div>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <div className="input_area">
+          <label>이메일</label>
+          <input
+            type="loginemail"
+            placeholder="이메일 입력"
+            
+            value={loginemail}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+          />
+        </div>
+
+        <div className="input_area">
+          <label>비밀번호</label>
+          <input
+            type="password"
+            placeholder="비밀번호 입력"
+            
+            value={loginpassword}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+          />
+        </div>
+
+       
+
+
+       
+
+        <button
+          style={{
+            opacity: isLoading ? 0.3 : 1,
+          }}>
+          {isLoading ? '로그인 중..' : '로그인'}
+        </button>
+        <div>
+          <Link href="/">돌아가기</Link>
+        </div>
+      </form>
+      <style jsx>
+        {`
+          .input_area input {
+            background-color: gray;
+          }
+        `}
+      </style>
+    </>
   );
 }
--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -5,22 +5,29 @@ export default function SignUp() {
   const [isLoading, setIsLoading] = useState(false);
   // const emailRef = useRef<HTMLInputElement>(null);
   // const pwdRef = useRef<HTMLInputElement>(null);
+  //const nameRef= useRef<HTMLInputElement>(null);
   // const pwdCheckRef = useRef<HTMLInputElement>(null);
+  //const school_nameRef = useRef<HTMLInputElement>(null);
 
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [passwordCheck, setPasswordCheck] = useState<string>('');
+  const[name, setName] = useState<string>('');
+  const[school_name,setSchool_name] = useState<string>('');
+  
+
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault();
     //로딩중이 아니고 입력 다 받으면 보내기
-    if (!isLoading && email && password) {
+    if (!isLoading && email && password &&name) {
       setIsLoading(true);
 
       // const email = emailRef.current.value;
       // const pwd = pwdRef.current.value;
       console.log(email);
       console.log(password);
+      console.log(name);
     }
     setIsLoading(false);
   }
@@ -66,6 +73,31 @@ export default function SignUp() {
             }}
           />
         </div>
+        <div className="input_area">
+          <label>이름</label>
+          <input
+            type="name"
+            placeholder="이름 입력"
+            // ref={nameRef}
+            value={name}
+            onChange={(event) => {
+              setPasswordCheck(event.target.value);
+            }}
+          />
+        </div>
+        <div className="input_area">
+          <label>학교</label>
+          <input
+            type="school_name"
+            placeholder="학교 입력"
+            // ref={school_nameRef}
+            value={school_name}
+            onChange={(event) => {
+              setPasswordCheck(event.target.value);
+            }}
+          />
+        </div>
+
 
         {/* 비밀번호 확인 */}
         <div>
