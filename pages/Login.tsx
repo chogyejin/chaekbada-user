7529a1c47a63f85a7dc1fca1c0caf1aeeda6de8c
chogyejin <whrpwls96@naver.com>
2021-10-22 00:18:55 +0900

    Login.tsx 공백 제거

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,52 +1,68 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-
-function Login() {
+export default function Login() {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
-  
- 
 
- 
-
+  console.log(email, password);
   return (
-      <div className="loginregister">
-        <form>
-            <div><input name="email" type="email" placeholder="이메일" value={email} onChange={(event)=>{
-            setEmail(event.target.value);
-          }} className="loginregister__input"/></div>
-            <div><input name="password" type="password" placeholder="비밀번호" value={password} onChange={(event)=>{
-            setPassword(event.target.value);
-          }} className="loginregister__input"/></div>
-            <div><button type="submit"  className="loginregister__button">로그인</button></div>
-        </form>
-        <style jsx>
+    <div className="loginregister">
+      <form>
+        <div>
+          <input
+            name="email"
+            type="email"
+            placeholder="이메일"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
+            className="loginregister__input"
+          />
+        </div>
+        <div>
+          <input
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            className="loginregister__input"
+          />
+        </div>
+        <div>
+          <button type="submit" className="loginregister__button">
+            로그인
+          </button>
+        </div>
+      </form>
+      <style jsx>
         {`
-        .loginregister{
-          display:flex;
-          align-items:center;
-          justify-content: center;
-          height:100vh;
-        }
-        .loginregister__input{
-          width:300px;
-          height:50px;
-          padding-left:10px;
-          marign:10px;
-        }
-        .loginregister__button{
-          background-color: rgb(248, 47, 98);
-          color: rgb(255, 255, 255);
-          font-weight: 700;
-          width: 100%;
-          border-radius: 40px;
-          height: 48px;
-          margin-top: 10px;
-        }
+          .loginregister {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+          }
+          .loginregister__input {
+            width: 300px;
+            height: 50px;
+            padding-left: 10px;
+            marign: 10px;
+          }
+          .loginregister__button {
+            background-color: rgb(248, 47, 98);
+            color: rgb(255, 255, 255);
+            font-weight: 700;
+            width: 100%;
+            border-radius: 40px;
+            height: 48px;
+            margin-top: 10px;
+          }
         `}
       </style>
-      </div>
-    );
-  }
-
-export default Login;
+    </div>
+  );
+}
