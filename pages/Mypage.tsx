1c8c209032682e3361be57b673ef62c805d09e1d
yatyat2 <xowls2287@naver.com>
2021-11-14 18:28:10 +0900

    reactstrap 설치

--- pages/Mypage.tsx
+++ pages/Mypage.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
+import { Button } from "reactstrap";
 
 export default () => {
-  
   return (
     <div>
-        안녕 나는 마이페이지
-
-
+      안녕 나는 마이페이지
+      <Button>힝힝</Button>
       <style jsx>{``}</style>
     </div>
   );
-}
+};
