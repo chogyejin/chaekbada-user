5a3356414814872f39054be15f41f240ecb9904a
chogyejin <whrpwls96@naver.com>
2021-11-21 21:23:21 +0900

    이메일 인증 페이지 api method 수정

--- pages/EmailVerification/[code].tsx
+++ pages/EmailVerification/[code].tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function EmailVerification({ isSuccessed }: Props) {
   return (
     <>
-      <div>이메일 인증 페이지</div>
+      <h1>이메일 인증 페이지</h1>
       <div>
         {isSuccessed === true ? (
           <div>이메일 인증 성공</div>
@@ -24,7 +24,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const verificationCode = context.params?.code;
   const result = await axiosFunction({
     url: '/signUp/verification',
-    method: 'GET',
+    method: 'POST',
     params: {
       token: verificationCode,
     },
@@ -32,7 +32,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   return {
     props: {
-      isSuccessed: result,
+      isSuccessed: result?.data,
     },
   };
 }
