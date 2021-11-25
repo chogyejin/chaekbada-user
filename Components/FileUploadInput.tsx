f84c3a97b5671fc1137413a6f96394af12c61f2f
GWLOCAL\32174918 <beewt@naver.com>
2021-11-25 20:26:07 +0900

    aws s3 문제 해결

--- Components/FileUploadInput.tsx
+++ Components/FileUploadInput.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import AWS from "aws-sdk";
-import { REGION, S3_BUCKET, S3_BUCKET_BASE_URL } from "../common/constant";
-import { Button, Input, InputGroup } from "reactstrap";
+import React, { useState } from 'react';
+import AWS from 'aws-sdk';
+import { REGION, S3_BUCKET, S3_BUCKET_BASE_URL } from '../common/constant';
+import { Button, Input, InputGroup } from 'reactstrap';
 
 interface IProps {
   onClickUpload: (uploadedBookImageUrl: string) => void;
@@ -11,8 +11,8 @@ export default function FileUploadInput(props: IProps) {
   const [progress, setProgress] = useState(0);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   AWS.config.update({
-    accessKeyId: "AKIAW3ZZO4Z4HOT7ISXF",
-    secretAccessKey: "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",
+    accessKeyId: 'AKIAW3ZZO4Z4P5KVF3Z2',
+    secretAccessKey: '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',
   });
   const myBucket = new AWS.S3({
     params: { Bucket: S3_BUCKET },
@@ -25,7 +25,7 @@ export default function FileUploadInput(props: IProps) {
 
   const uploadFile = (file: any) => {
     const params = {
-      ACL: "public-read",
+      ACL: 'public-read',
       Body: file,
       Bucket: S3_BUCKET,
       Key: file.name,
@@ -34,7 +34,7 @@ export default function FileUploadInput(props: IProps) {
 
     myBucket
       .putObject(params)
-      .on("httpUploadProgress", (evt) => {
+      .on('httpUploadProgress', (evt) => {
         setProgress(Math.round((evt.loaded / evt.total) * 100));
       })
       .send((err) => {
@@ -43,21 +43,20 @@ export default function FileUploadInput(props: IProps) {
   };
 
   return (
-    <div style={{marginBottom:"8px"}}>
+    <div style={{ marginBottom: '8px' }}>
       <InputGroup>
         <Input type="file" onChange={handleFileInput} />
         {progress !== 100 && (
           <Button
             onClick={() => uploadFile(selectedFile)}
-            disabled={!selectedFile}
-          >
-            {" "}
+            disabled={!selectedFile}>
+            {' '}
             이미지 업로드 하기
           </Button>
         )}
       </InputGroup>
       {selectedFile && (
-        <div style={{ textAlign: "left" }}>파일업로드 진행도: {progress}%</div>
+        <div style={{ textAlign: 'left' }}>파일업로드 진행도: {progress}%</div>
       )}
     </div>
   );
