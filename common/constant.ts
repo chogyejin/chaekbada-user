2a063f8975a33d41a3a042b7eb72c62f946540d3
yatyat2 <xowls2287@naver.com>
2021-11-27 17:03:52 +0900

    base endpoint 프로덕션과 dev 환경에서 각기 다르게 들어가도록 수정

--- common/constant.ts
+++ common/constant.ts
@@ -1,7 +1,7 @@
 export const IS_PRODUCTION =
   process.env.NODE_ENV === 'production' ? true : false;
 
-export const BACKEND_ENDPOINT = IS_PRODUCTION ? '' : 'http://localhost:4000';
+export const BACKEND_ENDPOINT = IS_PRODUCTION ? 'http://chaekbadabackend-env.eba-wmmnpxhk.ap-northeast-2.elasticbeanstalk.com' : 'http://localhost:4000';
 export const S3_BUCKET_BASE_URL =
   'https://chaekbada-image.s3.ap-northeast-2.amazonaws.com/';
 export const S3_BUCKET = 'chaekbada-image';
