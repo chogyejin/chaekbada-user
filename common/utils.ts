6e206a8f5542f1f093261948ac887ddf4051a905
chogyejin <whrpwls96@naver.com>
2021-11-29 06:19:18 +0900

    배포 테스트용 수정

--- common/utils.ts
+++ common/utils.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import router from 'next/router';
 import Cookies from 'universal-cookie';
-import { BACKEND_ENDPOINT } from './constant';
+// import { BACKEND_ENDPOINT } from './constant';
 
 export const GUEST_TOKEN =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imd1ZXN0QGd1ZXN0LmNvbSIsImlhdCI6MTYzNTg3NzgxOCwiaXNzIjoi7Yag7YGwIOuwnOq4ieyekCJ9.1y1sfa0uhjnGMKSjREjSWNTo7RSmHEva5RVufow-ADw';
@@ -27,7 +27,8 @@ export const axiosFunction = async (args: {
     switch (method) {
       case 'POST': {
         return await axios.post(
-          `${BACKEND_ENDPOINT}${url}`,
+          // `${BACKEND_ENDPOINT}${url}`,
+          `http://localhost:4000${url}`,
           {}, //2번째 파라미터는 request.body
           {
             headers, //headers는 3번째 파라미터
@@ -39,15 +40,20 @@ export const axiosFunction = async (args: {
       }
 
       case 'GET': {
-        return await axios.get(`${BACKEND_ENDPOINT}${url}`, {
-          params,
-          headers,
-        });
+        return await axios.get(
+          // `${BACKEND_ENDPOINT}${url}`,
+          `http://localhost:4000${url}`,
+          {
+            params,
+            headers,
+          },
+        );
       }
 
       case 'PUT': {
         return await axios.put(
-          `${BACKEND_ENDPOINT}${url}`,
+          // `${BACKEND_ENDPOINT}${url}`,
+          `http://localhost:4000${url}`,
           {},
           {
             headers,
