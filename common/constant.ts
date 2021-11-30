757f7878aa5b0b40d0f3ba6a5bcc1a7752823850
chogyejin <whrpwls96@naver.com>
2021-11-30 22:10:18 +0900

    백엔드 로컬로 변경

--- common/constant.ts
+++ common/constant.ts
@@ -4,8 +4,10 @@ export const IS_PRODUCTION =
 export const ACCESS_KEY_ID = process.env.NEXT_PUBLIC_ACCESS_KEY_ID;
 export const SECRET_ACCESS_KEY = process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY;
 
+// 'http://chaekbadabackend-env.eba-wmmnpxhk.ap-northeast-2.elasticbeanstalk.com';
+
 export const BACKEND_ENDPOINT = IS_PRODUCTION
-  ? 'http://chaekbadabackend-env.eba-wmmnpxhk.ap-northeast-2.elasticbeanstalk.com'
+  ? 'http://localhost:4000'
   : 'http://localhost:4000';
 export const S3_BUCKET_BASE_URL =
   'https://chaekbada-image.s3.ap-northeast-2.amazonaws.com/';
